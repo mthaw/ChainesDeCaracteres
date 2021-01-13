8098173d1ebf4ae7306a60e4f6c54bf2936dbe43
mthaw <mthaw@10.0.0.11>
2021-01-13 12:25:32 -0500

    Push

---
+++ Manipulation de chaines de characteres/src/WinningScore.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class WinningScore {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a3 = s.nextInt(), a2 = s.nextInt(), a1 = s.nextInt();
+		int b3 = s.nextInt(), b2 = s.nextInt(), b1 = s.nextInt();
+		int scorea = a3 * 3 + a2 * 2 + a1, scoreb = b3 * 3 + b2 * 2 + b1;
+		if (scorea > scoreb)
+			System.out.println("A");
+		else if (scoreb > scorea)
+			System.out.println("B");
+		else
+			System.out.println("T");
+	}
+}
