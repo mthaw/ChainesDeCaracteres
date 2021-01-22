9f0cdec3727ff4e7c1094cb5e509892c3c4b448f
mthaw <mthaw@10.0.0.11>
2021-01-22 13:43:14 -0500

    Essai

--- Manipulation de chaines de characteres/src/WinningScore.java
+++ Manipulation de chaines de characteres/src/WinningScore.java
@@ -7,7 +7,7 @@ public class WinningScore {
 		int b3 = s.nextInt(), b2 = s.nextInt(), b1 = s.nextInt();
 		int scorea = a3 * 3 + a2 * 2 + a1, scoreb = b3 * 3 + b2 * 2 + b1;
 		if (scorea > scoreb)
-			System.out.println("A");
+			System.out.println("A");   
 		else if (scoreb > scorea)
 			System.out.println("B");
 		else
